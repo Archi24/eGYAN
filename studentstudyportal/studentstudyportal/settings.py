63528e849cc9ded0c3828e55e42c470636465405
Archi mittal <mittalarchi2403@gmail.com>
2022-06-26 02:35:11 +0530

    hjmnjk

--- studentstudyportal/studentstudyportal/settings.py
+++ studentstudyportal/studentstudyportal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=[BASE_DIR/"static"]
 STATICFILES_DIRS=[BASE_DIR/"static"]
 CRISPY_TEMPLATE_PACK="bootstrap4"
 LOGIN_REDIRECT_URL='home'
@@ -129,3 +131,5 @@ LOGIN_URL='login'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#Activate Django-Heroku.
+django_heroku.settings(locals())
